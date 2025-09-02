fbb0b607dfb7fed64a3c0976a3cc029843540a1d
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2025-09-02 08:31:13 +0530

    Time: 2 ms (72.14%) | Memory: 61.6 MB (64.56%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buystock=prices[0];
-        int maxprofit=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]<buystock){
-                buystock=prices[i];
-            }
-            else
-            {
-                int currentprofit=prices[i]-buystock;
-                 maxprofit=Math.max(maxprofit,currentprofit);
-            }
+       int n=prices.length;
+       int buystock=prices[0];
+       int sellstock=0;
+       for(int i=1;i<n;i++){
+        if(prices[i]<buystock){
+            buystock=prices[i];
         }
-        return maxprofit;
+        sellstock=Math.max(prices[i]-buystock,sellstock);
+       } 
+       return sellstock;
     }
 }
