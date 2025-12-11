f8ae29fd74944a78632297ed38f082f83a536371
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2025-12-11 19:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (27.26%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      int sum=0;
-      int actualsum=0;
-      int result=0;
-      int n=nums.length;
-      for(int i=0;i<n;i++){
-        sum+=nums[i];
-        actualsum=n*(n+1)/2;
-        result=actualsum-sum;
-      } 
-      return result; 
-    }   
+        int n=nums.length;
+        int expectedsum=n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        expectedsum=expectedsum-sum;
+        return expectedsum;
+    }
 }
