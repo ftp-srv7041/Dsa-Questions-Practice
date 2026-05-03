5199a3dfeb4cd01831ca891c74f2eff3ff7346fa
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2026-05-03 19:39:58 +0530

    Time: 2 ms (99.94%) | Memory: 45.7 MB (71.31%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] freq=new int[256];
+        for(char c:t.toCharArray()){
+            freq[c]++;
+        }
+        int low=0;
+        int count=0;
+        int minlen=Integer.MAX_VALUE;
+        int start=0;
+        count=t.length();
+        for(int high=0;high<s.length();high++){
+            char ch=s.charAt(high);
+
+            if(freq[ch]>0){
+                count--;
+            }
+            freq[ch]--;
+
+            while(count==0){
+                if(high-low+1<minlen){
+                    minlen=high-low+1;
+                    start=low;
+                }
+                char left=s.charAt(low);
+                freq[left]++;
+
+                if(freq[left]>0){
+                    count++;
+                }
+                low++;
+            }
+        }
+        if(minlen==Integer.MAX_VALUE)
+        return "";
+        return s.substring(start,start+minlen);
+    }
+}
