703d4dfeba371d43e680b45e483e59f3fd54d3a7
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2026-05-03 13:20:01 +0530

    Time: 6 ms (9.10%) | Memory: 52.2 MB (54.18%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,20 +1,43 @@
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//        int low=0;
+//        int zerocount=0;
+//        int maxlen=0;
+//        for(int high=0;high<nums.length;high++){
+//         if(nums[high]==0){
+//         zerocount++;
+//         }
+//         while(zerocount>k){
+//             if(nums[low]==0){
+//                 zerocount--;
+//             }
+//             low++;
+//         }
+//         maxlen=Math.max(maxlen,high-low+1);
+//        } 
+//        return maxlen;
+//     }
+// }
+
+//  solved using integer mapping 
+
 class Solution {
     public int longestOnes(int[] nums, int k) {
        int low=0;
-       int zerocount=0;
-       int maxlen=0;
+       int ans=0;
+       int maxfreq=0;
+       int[] freq=new int[256];
        for(int high=0;high<nums.length;high++){
-        if(nums[high]==0){
-        zerocount++;
-        }
-        while(zerocount>k){
-            if(nums[low]==0){
-                zerocount--;
-            }
+        freq[nums[high]]++;
+        maxfreq=Math.max(maxfreq,freq[1]);
+        while((high-low+1)-maxfreq>k){
+            freq[nums[low]]--;
             low++;
         }
-        maxlen=Math.max(maxlen,high-low+1);
-       } 
-       return maxlen;
+        ans=Math.max(ans,high-low+1);
+       }
+       return ans;
     }
 }
+
+
