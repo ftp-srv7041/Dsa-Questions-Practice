893ad1fb08cae1294e36438c8fb5c4903b2f4258
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2026-05-12 17:41:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ArrayList<Integer> nextLargerElement(int[] nums) {
+        // code here
+        int n = nums.length;
+
+        int[] result = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        result[n - 1] = -1;
+
+        st.push(nums[n - 1]);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= nums[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.peek();
+            }
+
+            st.push(nums[i]);
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int num : result) {
+            ans.add(num);
+        }
+        return ans;
+    }
+}
