37481e2243f8d4e53d3c6706cf6ac3df84eec437
ftp-srv7041 <154993270+ftp-srv7041@users.noreply.github.com>
2026-05-28 17:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (13.84%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return helper(root,targetSum,0);
+    }
+        public boolean helper(TreeNode root,int targetSum,int sum){
+        if(root==null){
+            return false;
+        }
+        sum+=root.val;
+        // agar leaf node hai toh
+        if(root.left==null&&root.right==null){
+            if(sum==targetSum){
+                return true;
+            }
+        }
+        // agar leaf node nhi hai toh
+        boolean leftAns=helper(root.left,targetSum,sum);
+        boolean rightAns=helper(root.right,targetSum,sum);
+        return leftAns||rightAns;
+        } 
+}
